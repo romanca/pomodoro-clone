a7ec884371ca4f339728efc607fcc6e32eb3bf9d
romanca <r.jamrisko@gmail.com>
2021-08-26 14:16:27 +0200

    theme-provider

---
+++ src/Components/Layout.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+const Layout = () => {
+  return (
+    <div style={{ width: "100%", height: "100vh", background: "tomato" }}></div>
+  );
+};
+
+export default Layout;
